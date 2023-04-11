3ad9b6bd36f1800d282806bcddaabaca8781166c
Mateus Maranhão <feradourada@gmail.com>
2023-04-11 11:47:49 -0300

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Cadastro from '@/components/Cadastro.vue'
+import Index from '@/views/Index.vue'
+import Home from '@/components/Home.vue'
+import Login from '@/components/Login.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index,
+    redirect: '/home',
+    children: [
+
+      { 
+        path: 'cadastro', 
+        name: 'Cadastro', 
+        component: Cadastro 
+      },
+
+      { 
+        path: 'home', 
+        name: 'Home', 
+        component: Home 
+      },
+
+      { 
+        path: 'login', 
+        name: 'Login', 
+        component: Login 
+      },
+
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <navbar-index />
+  <h1>Index</h1>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import NavbarIndex from '@/components/NavbarIndex.vue'
+
+@Options({
+  components: {
+    NavbarIndex
+  }
+})
+export default class Home extends Vue {}
+</script>
