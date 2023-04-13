98a45ac89a13c8d5112e4d52be1db039b4161162
Mateus Maranhão <feradourada@gmail.com>
2023-04-12 22:00:49 -0300

    add connection with db with connection.php

---
+++ backend/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hotel_salvador";
+
+// Cria uma conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($db_conn->connect_error) {
+    die("DataBase Connection failed: " . $db_conn->connect_error);
+}
+
+?>
