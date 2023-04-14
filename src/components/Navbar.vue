513ee35df4920e6cecf86487d8e6dbdc6617aac4
Mateus Maranhão <feradourada@gmail.com>
2023-04-14 16:20:24 -0300

    Implement logout function

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-index bg-index">
+        <div class="container-fluid">
+            <router-link to="/" class="navbar-brand"><i class="fa-solid fa-umbrella-beach"></i> Hotel
+                Salvador</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <button @click="fazerLogout()" class="nav-link btn btn-link"><i
+                                class="fa-solid fa-arrow-right-from-bracket"></i> Logout</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import axios from 'axios'
+
+@Options({
+    components: {
+    },
+})
+export default class Navbar extends Vue {
+
+    fazerLogout() {
+        // Remove o token JWT do local storage
+        localStorage.removeItem('token')
+        // Remove o header de autorização do axios
+        delete axios.defaults.headers.common['Authorization']
+        // Redireciona o usuário de volta para a página de login
+        this.$router.push('/')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/navbar.scss';
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,19 @@
 <template>
+    <Navbar />
     <h1>Dashboard</h1>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Navbar from '@/components/Navbar.vue';
+
+@Options({
+    components: {
+        Navbar
+    },
+})
+export default class Dashboard extends Vue {
+
+    
+}
+</script>
